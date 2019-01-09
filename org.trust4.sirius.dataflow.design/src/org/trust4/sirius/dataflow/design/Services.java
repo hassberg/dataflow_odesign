974c189e11b88ef5f8928a9fbb1d0c7d37e2dafc
Tobias <tobias.hassberg@web.de>
2019-01-09 12:47:37 +0100

    added filters

--- org.trust4.sirius.dataflow.design/src/org/trust4/sirius/dataflow/design/Services.java
+++ org.trust4.sirius.dataflow.design/src/org/trust4/sirius/dataflow/design/Services.java
@@ -12,21 +12,6 @@ import edu.kit.ipd.sdq.dataflow.systemmodel.System;
  * The services class used by VSM.
  */
 public class Services {
-    public OperationCall getReturnValue(ReturnValueRef self) {
-	       
-	return self.getCall();
-	}
-
-	public Operation getReturnValueToCallee(ReturnValueRef self) {
-	       
-	return self.getCall().getCallee();
-	}
-
-	public Operation getReturnValueToCaller(ReturnValueRef self) {
-	       
-	return (Operation) self.getCall().getCaller();
-	}
-	
 	public String operationNumber(OperationCall call) {
 		Operation callee = call.getCallee();
 		Caller caller = call.getCaller();
@@ -39,16 +24,6 @@ public class Services {
 		}
 		return "0";
 	}    
-	
-	public String getcallReturnString(OperationCall call) {
-		String returnvalue ="";
-		//return call.getCallee().getReturnValueAssignments().get(0).getValue().getName();
-		for (VariableAssignment varassign : call.getCallee().getReturnValueAssignments()) {
-			returnvalue += varassign.getVariable().getName();
-			returnvalue += "\n";
-		}
-		return returnvalue;
-	}
 	public String getPreCallAssignmen(OperationCall call) {
 		String returnValue ="";
 		for(VariableAssignment preCallStateVar : call.getPreCallStateDefinitions()) {
@@ -78,27 +53,4 @@ public class Services {
 		}
 		return available;	
 	}
-
-
-	public String borderedNotes(Operation op) {
-	String returnValue ="";
-
-	for (Variable retVal : op.getReturnValues()) {
-		returnValue += retVal.getName()+ "\n";
-	}
-	
-	returnValue +="------ \n";
-	
-	for (PropertyDefinition propVal : op.getPropertyDefinitions()) {
-		returnValue += propVal.getProperty().getName() + "\n";
-	}
-	
-	return returnValue;
-	}
-
-
-
-
-
-
 }
