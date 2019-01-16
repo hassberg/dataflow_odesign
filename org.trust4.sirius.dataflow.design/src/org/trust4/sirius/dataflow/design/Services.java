cb8ca9646a9cf3193680f0bcbef26af6942ade13
Tobias <tobias.hassberg@web.de>
2019-01-16 16:56:56 +0100

    added simpler description for labels

--- org.trust4.sirius.dataflow.design/src/org/trust4/sirius/dataflow/design/Services.java
+++ org.trust4.sirius.dataflow.design/src/org/trust4/sirius/dataflow/design/Services.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 import org.eclipse.emf.ecore.EObject;
 
-
 import edu.kit.ipd.sdq.dataflow.systemmodel.*;
 import edu.kit.ipd.sdq.dataflow.systemmodel.System;
+import edu.kit.ipd.sdq.dataflow.systemmodel.provider.*;
+import edu.kit.ipd.sdq.dataflow.systemmodel.util.SystemModelAdapterFactory;
 
 /**
  * The services class used by VSM.
@@ -42,6 +43,16 @@ public class Services {
 		return returnValue;
 	}
 	
+	public String getReturnValueAssignment2(OperationCall call) {
+		String returnValue="";
+		VariableAssignmentItemProvider itemProvider = new VariableAssignmentItemProvider(new SystemModelAdapterFactory());
+		for (VariableAssignment var : call.getCallee().getReturnValueAssignments()) {
+			returnValue += itemProvider.getText(var) +"\n";
+		}
+		
+		return returnValue;
+	}
+	
 	public Collection<OperationCall> hasReturnValueAssignment(System system) {
 		Collection<OperationCall> available = new Vector<OperationCall>();
 		for (Operation operation : system.getOperations()) {
@@ -52,5 +63,25 @@ public class Services {
 			}
 		}
 		return available;	
+	}
+
+	public String getText(Operation op) {
+		OperationItemProvider itemProvider = new OperationItemProvider(new SystemModelAdapterFactory());
+		return itemProvider.getText(op);
+	}
+	
+	public String getSystemUsagesText(SystemUsage op) {
+		SystemUsageItemProvider itemProvider = new SystemUsageItemProvider(new SystemModelAdapterFactory());
+		return itemProvider.getText(op);
+	}
+
+	public String getPropertyDefinitionText(PropertyDefinition op) {
+		PropertyDefinitionItemProvider definitionItemProvider = new PropertyDefinitionItemProvider(new SystemModelAdapterFactory());
+		return definitionItemProvider.getText(op);
+	}
+
+	public String getReturnValueText(Variable op) {
+		VariableItemProvider itemProvider = new VariableItemProvider(new SystemModelAdapterFactory());
+		return itemProvider.getText(op);
 	}
 }
